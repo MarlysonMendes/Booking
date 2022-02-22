fec7cb2cf7fc82fb052226e083087a781ffa511a
Marlyson Mendes <marlysonmendes@gmail.com>
2022-02-22 17:54:57 -0300

    Models iniciais
    Add Models Hotel Room e Reservation no Domain

---
+++ CwkBooking.Domain/Models/Hotel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CwkBooking.Domain.Models
+{
+    public class Hotel
+    {
+        public Hotel(string name, int stars, string address)
+        {
+            if(string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("Hotel name is not allowed to be null or whitespace");
+        }
+        public int HotelId { get; set; }
+        public string Name { get; set; }
+        public int Stars { get; set; }
+        public string Address {get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Room> Rooms { get; set; }
+        public string Description { get; set; }
+    }
+}
